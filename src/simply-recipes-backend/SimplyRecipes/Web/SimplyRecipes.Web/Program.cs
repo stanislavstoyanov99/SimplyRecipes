9895dafb0586c5ae311c575b09333220000d5ae3
Stanislav Biserov Stoyanov <sstoyan@softserveinc.com>
2023-07-05 18:05:02 +0300

    Update to .NET 7.0

--- src/simply-recipes-backend/SimplyRecipes/Web/SimplyRecipes.Web/Program.cs
+++ src/simply-recipes-backend/SimplyRecipes/Web/SimplyRecipes.Web/Program.cs
@@ -1,20 +1,70 @@
 namespace SimplyRecipes.Web
 {
+    using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using System.Reflection;
+    using Newtonsoft.Json;
+
+    using SimplyRecipes.Models.ViewModels;
+    using SimplyRecipes.Services.Mapping;
+    using SimplyRecipes.Web.Infrastructure.Extensions;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder.Services, builder.Configuration);
+            var app = builder.Build();
+            Configure(app);
+            app.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddDatabase(configuration)
+                .AddCustomRouting()
+                .AddIdentity()
+                .AddAuthentication(services.GetApplicationConfig(configuration))
+                .AddCors(options =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    options.AddPolicy("AllowSpecificOrigin", builder =>
+                    builder
+                        .AllowCredentials()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .SetIsOriginAllowed((host) => true));
+                })
+                .AddApplicationServices(configuration)
+                .AddSwagger()
+                .AddControllers()
+                .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+        }
+
+        private static void Configure(WebApplication app)
+        {
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app
+               .UseSwaggerUI()
+               .UseRouting()
+               .UseCors("AllowSpecificOrigin")
+               .UseAuthentication()
+               .UseAuthorization()
+               .UseEndpoints(endpoints =>
+               {
+                   endpoints.MapControllers();
+               })
+               .ApplyMigrationsAndSeedDatabase();
+        }
     }
 }

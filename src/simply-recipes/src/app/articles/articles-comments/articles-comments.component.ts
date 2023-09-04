53252321b618a107ee36fd70136dabe75de451e6
stanislavstoyanov99 <45464117+stanislavstoyanov99@users.noreply.github.com>
2023-09-04 17:52:34 +0300

    Implement saving of sub article comments and add reply button

--- src/simply-recipes/src/app/articles/articles-comments/articles-comments.component.ts
+++ src/simply-recipes/src/app/articles/articles-comments/articles-comments.component.ts
@@ -1,14 +1,15 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faComments, faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faComments, faCalendarAlt, faReply } from '@fortawesome/free-solid-svg-icons';
 import { ArticleCommentsService } from 'src/app/services/article-comments.service';
 import { NgForm } from '@angular/forms';
 import { IArticleComment } from 'src/app/shared/interfaces/article-comments/article-comment';
 import { IPostArticleComment } from 'src/app/shared/interfaces/article-comments/post-article-comment';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/dialogs/error-dialog/error-dialog.component';
+import { debounceTime, fromEvent, take } from 'rxjs';
 
 @Component({
   selector: 'app-articles-comments',
@@ -24,14 +25,17 @@ export class ArticlesCommentsComponent implements OnInit {
   public isUserAuthenticated!: boolean;
   public returnUrl!: string;
 
+  private parentId: number | null = null;
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private library: FaIconLibrary,
     private articleCommentsService: ArticleCommentsService,
-    private dialog: MatDialog) {
+    private dialog: MatDialog,
+    private el: ElementRef) {
     this.returnUrl = this.router.routerState.snapshot.url;
-    this.library.addIcons(faComments, faCalendarAlt);
+    this.library.addIcons(faComments, faCalendarAlt, faReply);
   }
 
   ngOnInit(): void {
@@ -43,13 +47,23 @@ export class ArticlesCommentsComponent implements OnInit {
 
     const articleComment: IPostArticleComment = {
       articleId: this.articleId,
-      parentId: null,
+      parentId: this.parentId,
       content: form.value['content']
     };
 
+    this.postComment(form, articleComment);
+  }
+
+  onReplyCommentHandler(commentId: number): void {
+    this.scrollToCommentSubmitForm();
+    this.parentId = commentId;
+  }
+
+  private postComment(form: NgForm, articleComment: IPostArticleComment): void {
     this.articleCommentsService.postComment(articleComment).subscribe({
       next: (articleComment) => {
         this.articleComments = [...this.articleComments, articleComment];
+        this.parentId = null;
         form.reset();
       },
       error: (err: string) => {
@@ -60,5 +74,32 @@ export class ArticlesCommentsComponent implements OnInit {
         });
       }
     });
+  }
+
+  private scrollToCommentSubmitForm(): void {
+    const commentFormElement: HTMLElement = this.el.nativeElement.querySelector(
+      "#comment-form"
+    );
+    const commentTextareaElement: HTMLElement = this.el.nativeElement.querySelector(
+      "#content-textarea"
+    );
+
+    window.scroll({
+      top: this.getTopOffset(commentFormElement),
+      left: 0,
+      behavior: "smooth"
+    });
+
+    fromEvent(window, "scroll")
+      .pipe(
+        debounceTime(100),
+        take(1)
+      )
+      .subscribe(() => commentTextareaElement.focus());
+  }
+
+  private getTopOffset(controlEl: HTMLElement): number {
+    const labelOffset = 100;
+    return controlEl.getBoundingClientRect().top + window.scrollY - labelOffset;
   }
 }

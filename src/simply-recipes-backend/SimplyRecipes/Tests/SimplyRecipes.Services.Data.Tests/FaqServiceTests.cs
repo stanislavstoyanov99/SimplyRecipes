e16367e5e44eb2776a86d2e86ffd144842cd4ea2
stanislavstoyanov99 <45464117+stanislavstoyanov99@users.noreply.github.com>
2023-11-10 15:56:22 +0200

    Add unit tests for FaqService

--- src/simply-recipes-backend/SimplyRecipes/Tests/SimplyRecipes.Services.Data.Tests/FaqServiceTests.cs
+++ src/simply-recipes-backend/SimplyRecipes/Tests/SimplyRecipes.Services.Data.Tests/FaqServiceTests.cs
@@ -4,11 +4,14 @@ namespace SimplyRecipes.Services.Data.Tests
 
     using Microsoft.Data.Sqlite;
     using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
 
     using SimplyRecipes.Data;
     using SimplyRecipes.Data.Models;
     using SimplyRecipes.Data.Repositories;
     using SimplyRecipes.Models.InputModels.Administration.Faq;
+    using SimplyRecipes.Models.ViewModels.Faq;
+    using SimplyRecipes.Services.Data.Common;
     using SimplyRecipes.Services.Data.Interfaces;
     using SimplyRecipes.Services.Mapping;
 
@@ -43,6 +46,159 @@ namespace SimplyRecipes.Services.Data.Tests
             Assert.Equal(1, count);
         }
 
+        [Fact]
+        public async Task CheckSettingFaqEntryProperties()
+        {
+            var model = new FaqCreateInputModel
+            {
+                Question = "What do you think about the website?",
+                Answer = "I think it's very good."
+            };
+
+            await this.faqService.CreateAsync(model);
+
+            var faqEntry = await this.faqEntriesRepository.All().FirstOrDefaultAsync();
+
+            Assert.Equal(model.Question, faqEntry.Question);
+            Assert.Equal(model.Answer, faqEntry.Answer);
+        }
+
+        [Fact]
+        public async Task CheckIfAddingFaqEntryThrowsArgumentException()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var faqEntry = new FaqCreateInputModel
+            {
+                Question = this.firstFaqEntry.Question,
+                Answer = this.firstFaqEntry.Answer,
+            };
+
+            var exception = await Assert
+                .ThrowsAsync<ArgumentException>(async () => await this.faqService.CreateAsync(faqEntry));
+
+            Assert.Equal(string.Format(
+                ExceptionMessages.FaqAlreadyExists, faqEntry.Question, faqEntry.Answer), exception.Message);
+        }
+
+        [Fact]
+        public async Task CheckIfAddingFaqEntryReturnsViewModel()
+        {
+            var faqEntry = new FaqCreateInputModel
+            {
+                Question = "What do you think about our reviews?",
+                Answer = "There are good.",
+            };
+
+            var viewModel = await this.faqService.CreateAsync(faqEntry);
+            var dbEntry = await this.faqEntriesRepository.All().FirstOrDefaultAsync();
+
+            Assert.Equal(dbEntry.Id, viewModel.Id);
+            Assert.Equal(dbEntry.Question, viewModel.Question);
+            Assert.Equal(dbEntry.Answer, viewModel.Answer);
+        }
+
+        [Fact]
+        public async Task CheckIfDeletingFaqEntryWorksCorrectly()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            await this.faqService.DeleteByIdAsync(this.firstFaqEntry.Id);
+
+            var count = await this.faqEntriesRepository.All().CountAsync();
+
+            Assert.Equal(0, count);
+        }
+
+        [Fact]
+        public async Task CheckIfDeletingFaqEntryReturnsNullReferenceException()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var exception = await Assert
+                .ThrowsAsync<NullReferenceException>(async () => await this.faqService.DeleteByIdAsync(2));
+
+            Assert.Equal(string.Format(ExceptionMessages.FaqNotFound, 2), exception.Message);
+        }
+
+        [Fact]
+        public async Task CheckIfEditingFaqEntryWorksCorrectly()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var faqEditViewModel = new FaqEditViewModel
+            {
+                Id = this.firstFaqEntry.Id,
+                Answer = "Changed Answer",
+                Question = "Changed Question",
+            };
+
+            await this.faqService.EditAsync(faqEditViewModel);
+
+            Assert.Equal(faqEditViewModel.Answer, this.firstFaqEntry.Answer);
+            Assert.Equal(faqEditViewModel.Question, this.firstFaqEntry.Question);
+        }
+
+        [Fact]
+        public async Task CheckIfEditingFaqEntryReturnsNullReferenceException()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var faqEditViewModel = new FaqEditViewModel
+            {
+                Id = 2
+            };
+
+            var exception = await Assert
+                .ThrowsAsync<NullReferenceException>(async () => await this.faqService.EditAsync(faqEditViewModel));
+
+            Assert.Equal(string.Format(ExceptionMessages.FaqNotFound, faqEditViewModel.Id), exception.Message);
+        }
+
+        [Fact]
+        public async Task CheckIfGetAllAsyncWorksCorrectly()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var result = await this.faqService.GetAllAsync<FaqDetailsViewModel>();
+
+            var count = result.Count();
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public async Task CheckIfGetFaqViewModelByIdAsyncWorksCorrectly()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var expectedModel = new FaqDetailsViewModel
+            {
+                Id = this.firstFaqEntry.Id,
+                Answer = this.firstFaqEntry.Answer,
+                Question = this.firstFaqEntry.Question,
+            };
+
+            var viewModel = await this.faqService
+                .GetViewModelByIdAsync<FaqDetailsViewModel>(this.firstFaqEntry.Id);
+
+            var expected = JsonConvert.SerializeObject(expectedModel);
+            var actual = JsonConvert.SerializeObject(viewModel);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task CheckIfGetViewModelByIdAsyncThrowsNullReferenceException()
+        {
+            await this.SeedFaqEntriesAsync();
+
+            var exception = await Assert
+                .ThrowsAsync<NullReferenceException>(
+                    async () => await this.faqService.GetViewModelByIdAsync<FaqDetailsViewModel>(2));
+
+            Assert.Equal(string.Format(ExceptionMessages.FaqNotFound, 2), exception.Message);
+        }
+
         public async ValueTask DisposeAsync()
         {
             await this.connection.CloseAsync();
@@ -70,7 +226,7 @@ namespace SimplyRecipes.Services.Data.Tests
             };
         }
 
-        private async Task SeedFaqEntries()
+        private async Task SeedFaqEntriesAsync()
         {
             await this.faqEntriesRepository.AddAsync(this.firstFaqEntry);
             await this.faqEntriesRepository.SaveChangesAsync();
